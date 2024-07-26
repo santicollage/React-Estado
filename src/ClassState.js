cbb4e06852ee707cb33216625171780130dc04ab
Santiago Urrego <santiagourregolaya@gmail.com>
2024-07-26 11:29:37 -0500

    Usando estados compuestos

--- src/ClassState.js
+++ src/ClassState.js
@@ -1,15 +1,55 @@
 import React from "react";
+import { Loading } from "./Loading.js";
+
+const SECURITY_CODE = 'paradigma';
 
 class ClassState extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      value: '',
+      error: false,
+      loading: false,
+    };
+  }
+
+  componentDidUpdate() {
+    if(this.state.loading) {
+      setTimeout(() => {
+        if (this.state.value === SECURITY_CODE) {
+          this.setState({error: false, loading: false});
+        } else {
+          this.setState({error: true, loading: false});
+        }
+      }, 3000);
+    }
+  }
+
   render() {
     return (
       <div>
-        <h2>Eliminar ClassState</h2>
+        <h2>Eliminar {this.props.name}</h2>
 
         <p>Por favor, escribe el codigo de seguridad.</p>
 
-        <input placeholder="Código de seguridad"/>
-        <button>Comprobar</button>
+        {(this.state.error && !this.state.loading) && (
+          <p>Error: El código es incorrecto</p>
+        )}
+        {this.state.loading && (
+          <Loading/>
+        )}
+
+        <input 
+          placeholder="Código de seguridad"
+          value={this.state.value}
+          onChange={(event) => {
+            this.setState({value: event.target.value})
+          }}
+        />
+        <button
+          onClick={() => this.setState({loading: true})}
+        >Comprobar</button>
 
       </div>
     )
--- src/UseState.js
+++ src/UseState.js
@@ -1,14 +1,63 @@
 import React from "react";
 
-function UseState() {
+const SECURITY_CODE = 'paradigma';
+
+function UseState({ name }) {
+  const [state, setState] = React.useState({
+    value: '',
+    error: false,
+    loading: false,
+  });
+
+  React.useEffect(() => {
+    if(state.loading) {
+      setTimeout(() => {
+        if (state.value === SECURITY_CODE) {
+          setState({
+            ...state,
+            loading: false,
+            error: false
+          });
+        } else {
+          setState({
+            ...state,
+            loading: false,
+            error: true
+          });
+        }
+      }, 3000);
+    }
+  }, [state.loading]);
+
   return (
     <div>
-      <h2>Eliminar UseState</h2>
+      <h2>Eliminar {name}</h2>
 
       <p>Por favor, escribe el codigo de seguridad.</p>
 
-      <input placeholder="Código de seguridad"/>
-      <button>Comprobar</button>
+      {(state.error && !state.loading) && (
+        <p>Error: El código es incorrecto</p>
+      )}
+
+      {state.loading && (
+        <p>Cargando...</p>
+      )}
+
+      <input 
+        placeholder="Código de seguridad"
+        value={state.value}
+        onChange={(event) => {
+          setState({
+            ...state,
+            value: event.target.value
+          })
+        }}
+      />
+      <button
+        onClick={() => setState({
+          ...state,
+          loading: true})}
+      >Comprobar</button>
 
     </div>
   )
